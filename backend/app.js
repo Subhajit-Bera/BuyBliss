273521faec7303088efe408c179a940638366ac5
Subhajit-Bera <103336107+Subhajit-Bera@users.noreply.github.com>
2023-09-17 14:13:03 +0530

    cretingfirstAPI

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
+app.use(express.json())
+
+
+//Import routes
+const router=require("./routes/productRoute");
+app.use("/api/v1",router);
+
 
 module.exports=app;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const app = require("./app");
 
+const dotenv=require("dotenv");
+
+
+//Config : for knowing the server the PORT number define in config folder
+dotenv.config({path:"backend/config/config.env"})
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
